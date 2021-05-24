59216d40a4235aa38c458c19aad91947a04747f2
Ron Shafii <ron.shafii@metaltoad.com>
2021-05-24 16:39:23 -0700

    updating self assertions for view bug testing

--- cloudbolt/problem_1.py
+++ cloudbolt/problem_1.py
@@ -18,20 +18,16 @@ class BugTrackerService():
 
 class CloudboltTests():
 
-    bug = {
-    "bug_id": 1234 ,
-    "title": "The service is broken",
-    "description": "I tried this thing and it broke",
-    "status": "OPEN",
-    }
 
     def test_add_bug(self):
     """
-    Add a bug, verify the title and desc aren't empty and verify the bug ID is unique
+    Add a bug, verify the title and desc aren't empty and verify the bug ID returned is unique
     """
 
+    bug['title'] = 'API 404 error'
     title = bug['title']
-    description = bug ['description']
+    bug['description'] = 'webpage error'
+    description = bug['description']
     # verify title isn't missing
     if self.bug['title'] == None:
         print("Missing title")
@@ -44,15 +40,26 @@ class CloudboltTests():
 
     def test_view_bug(self):
     """
-    View bug info is returned as dictionary
+    Lookup bug 1234 and verify the response is the same as the sample collection
     """
+    expected = {
+    "bug_id": 1234 ,
+    "title": "The service is broken",
+    "description": "I tried this thing and it broke",
+    "status": "OPEN",
+    }
 
     bug_id = bug[1234]
-    # lookup bug 1234 and verify it contains the same info as the orignal collection
     result = self.BugTrackerService.view_bug(bug_id)
-    expected = self.bug
-    assert self.result == self.bug
+    assert self.result == self.expected
 
 
+    def test_find_bug_by_name(self):
+        """
+        Verify performing a lookup by title returns based on its name
+        """
 
-
+    def test_edit_bug_description(self):
+        """
+        verify if you can edit a bug description that it retains its state
+        """
