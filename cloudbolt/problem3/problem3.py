b853182e2076c57a34d0ffbc8a399988a79df578
Ron Shafii <ron.shafii@metaltoad.com>
2021-05-26 14:14:09 -0700

    updating NewbugPage methods

--- cloudbolt/problem3/problem3.py
+++ cloudbolt/problem3/problem3.py
@@ -3,9 +3,9 @@ from selenium import webdriver
 
 class NewBugPage(BasePage): 
     driver = webdriver.Chrome()
-    def fill_form(title:str, description:str): 
-        driver.find_element_by_name("Title").send_keys("This is a unique but title")
-        driver.find_element_by_name("Description").send_keys("This is some unique bug description")
+    def fill_form(title, description): 
+        driver.find_element_by_name("Title").send_keys(title)
+        driver.find_element_by_name("Description").send_keys(description)
 
     def submit_new_bug(): 
        driver.find_element_by_name("submit").click()
