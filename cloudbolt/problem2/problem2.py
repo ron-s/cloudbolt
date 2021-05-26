1123cb5d197873fdc49fa23ae94b9d439352f9ee
Ron Shafii <ron.shafii@metaltoad.com>
2021-05-25 17:42:05 -0700

    adding test create new bug

--- cloudbolt/problem2/problem2.py
+++ cloudbolt/problem2/problem2.py
@@ -4,6 +4,21 @@ import requests
 
 url =  https://bugs.info:4000/api/v1/bug
 
-r = requests.put(url)
+
+def test_create_new_bug():
+    """ 
+    Create a new bug and verify sucessful 200 response
+    """
+
+    data = {
+        "title": "The service is broken",
+        "description": "I tried this thing and it broke",
+    }
+
+    r = requests.put(url, data)
+    # verify 200 request status code
+    assert response.status_code == 200
+    
+
 
 
