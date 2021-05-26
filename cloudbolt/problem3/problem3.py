782ba11e9d7e7a07b3f54c5537c25a46490f51b9
Ron Shafii <ron.shafii@metaltoad.com>
2021-05-26 14:30:53 -0700

    updating NewbugPage methods

--- cloudbolt/problem3/problem3.py
+++ cloudbolt/problem3/problem3.py
@@ -8,7 +8,7 @@ class NewBugPage(BasePage):
         driver.find_element_by_name('Description').send_keys(description)
 
     def submit_new_bug(): 
-       driver.find_element_by_name('Next').click()
+        driver.find_element_by_name('Next').click()
 
     def cancel_bug_submission():
         driver.find_element_by_name('Cancel').click()
