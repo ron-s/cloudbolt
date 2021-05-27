7b534e5ce499ef3bbe37295da3a4f9d76e207bb4
Ron Shafii <ron.shafii@metaltoad.com>
2021-05-27 16:32:27 -0700

    updating directory

---
+++ cloudbolt/problem3/pages/new_bug_page.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from Pages.base_page import BasePage
+import pytest
+
+
+class NewBugPage(BasePage):
+     
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    def fill_form(title, description): 
+        driver.find_element_by_name('Title').send_keys(title)
+        driver.find_element_by_name('Description').send_keys(description)
+
+    def submit_new_bug(): 
+        driver.find_element_by_name('Next').click()
+
+    def cancel_bug_submission():
+        driver.find_element_by_name('Cancel').click()
