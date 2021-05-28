c308ef248a32d81e55db9cbd2acf53f4d5cfc524
Ron Shafii <ron.shafii@metaltoad.com>
2021-05-27 19:32:13 -0700

    updating missing code

---
+++ cloudbolt/problem3/Pages/bug_page.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from Base.base_page import BasePage
+import pytest
+
+
+class NewBugPage(BasePage):
+     
+    driver = webdriver.Chrome(self)
+
+    def fill_form(title, description): 
+        driver.find_element_by_name('Title').clear()
+        driver.find_element_by_name('Title').send_keys(title)
+        driver.find_element_by_name('Description').clear()
+        driver.find_element_by_name('Description').send_keys(description)
+
+    def submit_new_bug(): 
+        driver.find_element_by_name('Next').click()
+
+    def cancel_bug_submission():
+        driver.find_element_by_name('Cancel').click()
+
+    
